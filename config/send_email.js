15164191bbbf6b0027526b22cce673aee80b8abe
olololoe <olololoe1001st2@gmail.com>
2020-11-14 12:21:02 +0700

    Fix/send_mail_2

--- config/send_email.js
+++ config/send_email.js
@@ -2,19 +2,32 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const { OAuth2 } = google.auth;
 const { handleMail, handleFailed } = require("../controllers/user/middleware");
-const REDIRECT_URL =  "https://developers.google.com/oauthplayground"
+const REDIRECT_URL = "https://developers.google.com/oauthplayground";
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  REDIRECT_URL
+);
 
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken();
 exports.sendEmail = async (email, res, code) => {
   let mailTransporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
+      accessType: "offline",
       user: process.env.EMAIL,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: process.env.ACCESS_TOKEN,
+      accessToken: accessToken,
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   let mailDetails = {
@@ -22,6 +35,7 @@ exports.sendEmail = async (email, res, code) => {
     to: email,
     subject: "Lovely: verify email address",
     html: `<h3>Please confirm your email address </h3> </hr> <h5>Your code: </h5> <h1>${code}</h1>`,
+    generateTextFromHTML: true,
   };
 
   mailTransporter.sendMail(mailDetails, (err, _) => {

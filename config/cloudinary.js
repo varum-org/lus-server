1e87e2160f14b598dca23593a54bc05da8bc7d37
Duy Nguyen Ngoc <duynn@nal.vn>
2022-11-10 22:53:52 +0700

    update url hbs

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "employeern",
+  cloud_name: "lusapp",
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
